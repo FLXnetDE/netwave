e02c27f6a862f73eaf13b4dcc6f8f30e61ecec0f
Felix <felix@junger-dettenhausen.de>
2025-07-08 18:01:08 +0200

    feat: implementing client cleanup on inactivity, needs testing and better purpose, closes #13

--- src/client/client-registry.ts
+++ src/client/client-registry.ts
@@ -1,8 +1,27 @@
+import { channelManager } from '../channel/channel-manager';
+import logger from '../util/logger';
 import { Client } from './client';
+
+const INACTIVITY_TIMEOUT: number = 30_000; // 30 seconds
 
 class ClientRegistry {
     private clients = new Map<number, Client>();
     private nextClientId = 1;
+
+    constructor() {
+        setInterval(this.cleanupInactiveClients.bind(this), 10_000);
+    }
+
+    cleanupInactiveClients(): void {
+        const now = Date.now();
+        for (const [clientId, client] of this.clients.entries()) {
+            if (now - client.lastActivity > INACTIVITY_TIMEOUT) {
+                console.log(`[Cleanup] Removing inactive client ${clientId}`);
+                this.clients.delete(clientId);
+                channelManager.leaveChannel(clientId);
+            }
+        }
+    }
 
     getClients(): Client[] {
         return Array.from(this.clients.values());
